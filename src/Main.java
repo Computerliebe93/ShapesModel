85c6afb90e1298ad5dd2ece2f5fc30ca6a5815c4
Computerliebe93 <70689092+Computerliebe93@users.noreply.github.com>
2021-01-03 12:46:49 +0100

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -32,115 +32,115 @@ public class Main {
 
     }
 }
-    // Abstract class for euclidean distance
-    abstract class EuclideanDistanceTo{
+// Abstract class for euclidean distance
+abstract class EuclideanDistanceTo{
     abstract void EuclideanDistance();
-    }
+}
 
-    // abstract class for shapes
-    abstract class Shapes{
+// abstract class for shapes
+abstract class Shapes{
     abstract void Center();
     abstract void Area();
     abstract void Circumference();
 }
 
-    class Rectangle1 extends Shapes {
-        // Length
-        int X1 = 0;
-        int X2 = 8;
-        // Width
-        int Y1 = 0;
-        int Y2 = 4;
+class Rectangle1 extends Shapes {
+    // Length
+    int X1 = 0;
+    int X2 = 8;
+    // Width
+    int Y1 = 0;
+    int Y2 = 4;
 
-        @Override
-        void Center() {
-            System.out.println("Center = " + (X1 + X2 / 2) + (Y1 + Y2 / 2));
-        }
-
-        @Override
-        void Area() {
-            System.out.println("Area = " + (X1 + X2) * (Y1 + Y2));
-        }
-
-        @Override
-        void Circumference() {
-            System.out.println("Circumference = " + (2 * ((X2 - X1) + (Y2 - Y1))));
-        }
+    @Override
+    void Center() {
+        System.out.println("Center = " + (X1 + X2 / 2) + (Y1 + Y2 / 2));
     }
 
-        class Triangle1 extends Shapes {
-            // Base
-            int X1 = 8;
-            int X2 = 16;
+    @Override
+    void Area() {
+        System.out.println("Area = " + (X1 + X2) * (Y1 + Y2));
+    }
 
-            // Sides
-            int Y1 = 4;
-            int X3 = 4;
+    @Override
+    void Circumference() {
+        System.out.println("Circumference = " + (2 * ((X2 - X1) + (Y2 - Y1))));
+    }
+}
 
-            @Override
-            void Center() {
-                System.out.println("Center = " + X1 + " - " + X2 + " - " + Y1 + " - " + X3);
-            }
+class Triangle1 extends Shapes {
+    // Base
+    int X1 = 8;
+    int X2 = 16;
 
-            @Override
-            void Area() {
-                System.out.println("Area = " + Y1 * (X2 - X1) / 2);
-            }
+    // Sides
+    int Y1 = 4;
+    int X3 = 4;
 
-            @Override
-            void Circumference() {
-                System.out.println("Circumference = " + (X2 + X1 + X3 + Y1));
-            }
-        }
+    @Override
+    void Center() {
+        System.out.println("Center = " + X1 + " - " + X2 + " - " + Y1 + " - " + X3);
+    }
 
-        class Circle1 extends Shapes {
-            // Radius
-            float radius = 5;
+    @Override
+    void Area() {
+        System.out.println("Area = " + Y1 * (X2 - X1) / 2);
+    }
 
-            // Center
-            int Circ1CentX = 5;
-            int CircCentY = 5;
+    @Override
+    void Circumference() {
+        System.out.println("Circumference = " + (X2 + X1 + X3 + Y1));
+    }
+}
 
-            @Override
-            void Center() {
-                System.out.println("Center = " + Circ1CentX + "," + CircCentY);
-                }
+class Circle1 extends Shapes {
+    // Radius
+    float radius = 5;
 
-            @Override
-            void Area() {
-                System.out.println("Area = " + (Math.PI * Math.pow(radius, 2)));
-            }
+    // Center
+    int Circ1CentX = 5;
+    int CircCentY = 5;
 
-            @Override
-            void Circumference() {
-                System.out.println("Circumference = " + (2 * radius * Math.PI));
-            }
-        }
+    @Override
+    void Center() {
+        System.out.println("Center = " + Circ1CentX + "," + CircCentY);
+    }
 
-            class Distance extends EuclideanDistanceTo {
-                // Rectangle1 center
-                int Rect1CentX = 8;
-                int Rect1CentY = 4;
+    @Override
+    void Area() {
+        System.out.println("Area = " + (Math.PI * Math.pow(radius, 2)));
+    }
 
-                // Circle1 center
-                int Circ1CentX = 5;
-                int Circ1CentY = 5;
+    @Override
+    void Circumference() {
+        System.out.println("Circumference = " + (2 * radius * Math.PI));
+    }
+}
 
-                // Triangle1 center
-                int TriCentX1 = 8;
-                int TriCentX2 = 16;
-                int TriCentY1 = 4;
-                int TriCentY2 = 4;
+class Distance extends EuclideanDistanceTo {
+    // Rectangle1 center
+    int Rect1CentX = 8;
+    int Rect1CentY = 4;
 
-                @Override
-                void EuclideanDistance() {
-                    System.out.println("Euclidean distance from Triangle1 to Rectangle1 = "
-                    + Math.sqrt(Math.pow(TriCentX1 - TriCentX2 - TriCentY1 - TriCentY2, 2) + Math.pow(Rect1CentX - Rect1CentY, 2)));
+    // Circle1 center
+    int Circ1CentX = 5;
+    int Circ1CentY = 5;
 
-                    System.out.println("Euclidean distance from Rectangle1 to Circle1 = "
-                            + Math.sqrt(Math.pow(Rect1CentX - Rect1CentY, 2) + Math.pow(Circ1CentX - Circ1CentY, 2)));
+    // Triangle1 center
+    int TriCentX1 = 8;
+    int TriCentX2 = 16;
+    int TriCentY1 = 4;
+    int TriCentY2 = 4;
 
-                    System.out.println("Euclidean distance from Triangle1 to Circle1 = "
-                            + Math.sqrt(Math.pow(TriCentX1 - TriCentX2 - TriCentY1 - TriCentY2, 2) + Math.pow(Circ1CentX - Circ1CentY, 2)));
-                }
-            }
+    @Override
+    void EuclideanDistance() {
+        System.out.println("Euclidean distance from Triangle1 to Rectangle1 = "
+                + Math.sqrt(Math.pow(TriCentX1 - TriCentX2 - TriCentY1 - TriCentY2, 2) + Math.pow(Rect1CentX - Rect1CentY, 2)));
+
+        System.out.println("Euclidean distance from Rectangle1 to Circle1 = "
+                + Math.sqrt(Math.pow(Rect1CentX - Rect1CentY, 2) + Math.pow(Circ1CentX - Circ1CentY, 2)));
+
+        System.out.println("Euclidean distance from Triangle1 to Circle1 = "
+                + Math.sqrt(Math.pow(TriCentX1 - TriCentX2 - TriCentY1 - TriCentY2, 2) + Math.pow(Circ1CentX - Circ1CentY, 2)));
+    }
+}
